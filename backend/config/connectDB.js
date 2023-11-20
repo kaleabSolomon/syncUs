80f946e13a62feb1a07262569aa47012da11a038
Kaleab Solomon <kaleabslmn@gmail.com>
2023-11-20 17:40:02 +0300

    connect to database

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const DB = await process.env.DATABASE_URL.replace(
-    "<password>",
-    process.env.DATABASE_PASSWORD
-  );
-  console.log(DB);
+  try {
+    const DB = process.env.DATABASE_URL.replace(
+      "<password>",
+      process.env.DATABASE_PASSWORD
+    );
+    const connect = await mongoose.connect(DB);
+    console.log("Connected to database");
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
