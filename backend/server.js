3fbda737cdeddd565327eed7ed4c5101c84ac037
Kaleab Solomon <kaleabslmn@gmail.com>
2023-11-20 17:06:44 +0300

    exit when general exceptions occur

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const dotenv = require("dotenv").config();
 const app = require("./app");
 
+process.on("uncaughtException", (err) => {
+  console.log("uncaught Exception");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("server listening on port " + port);
