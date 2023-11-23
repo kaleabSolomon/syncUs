fb48de06898edd1cae2ad735de995a2c18ccf640
Kaleab Solomon <kaleabslmn@gmail.com>
2023-11-23 22:01:22 +0300

    send jwt when signing up

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,12 @@
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.signUp = asyncHandler(async (req, res) => {
   const { name, password, email } = req.body;
@@ -22,10 +29,12 @@ exports.signUp = asyncHandler(async (req, res) => {
       profileImage: req.body.profileImage,
       role: req.body.role,
     });
+    const token = signToken(newUser._id);
     res.status(201).json({
       status: "success",
       name: newUser.name,
       _id: newUser.id,
+      token,
     });
   } catch (err) {
     res.status(400);
