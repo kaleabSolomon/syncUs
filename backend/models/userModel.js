f589e0540d7aa3814b70e6250c2556a5c318c357
Kaleab Solomon <kaleabslmn@gmail.com>
2023-11-23 19:17:34 +0300

    add middleware to hash passwords when user created

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -40,4 +41,13 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirmation = undefined;
+});
+
 const User = mongoose.model("User", userSchema);
+
+module.exports = User;
