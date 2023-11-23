9a17e180844aee8b908ef82f71397f50c7ef7d44
Kaleab Solomon <kaleabslmn@gmail.com>
2023-11-23 18:58:14 +0300

    check if user already exists

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,15 +4,16 @@ const User = require("../models/userModel");
 exports.signUp = asyncHandler(async (req, res) => {
   const { name, password, email } = req.body;
   if (!name || !password || !email) {
-    console.log("missing");
     res.status(400);
     throw new Error("all fields are required");
   }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
   res.status(201).json({
     status: "success",
-    name: name,
-    password: password,
-    email: email,
     message: "user created",
   });
 });
