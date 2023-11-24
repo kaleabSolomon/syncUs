25c10f647d75c0360c557e7ec89189fd0d0aaeb6
Kaleab Solomon <kaleabslmn@gmail.com>
2023-11-24 18:11:59 +0300

    define login route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 router.route("/signUp").post(authController.signUp);
+router.route("/login").post(authController.login);
 
 module.exports = router;
