b5bf3bdeb53ca25ce1eda7b0c495c08ae7a9119e
Kaleab Solomon <kaleabslmn@gmail.com>
2023-11-27 18:49:45 +0300

    create a contacts model

---
+++ backend/models/contactModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  name: {
+    type: String,
+    required: [true, "please add a contact name"],
+  },
+  phone: {
+    type: String,
+    required: [true, "please add the phone number of the contact"],
+  },
+  address: {
+    type: String,
+  },
+  relationShip: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Contact", contactSchema);
